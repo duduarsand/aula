c9b985dd93449227cccd5788334f4ed866bb4b21
Eduardo Vinicius Arsand <EArsand@edu.unisinos.br>
2023-03-06 22:10:29 -0300

    oi

---
+++ parapraticar.py
@@ -0,0 +1,11 @@
+a = float(input("insira um número:"))
+b = int(input("insira um número:"))
+
+
+
+
+
+print(f"{a} multiplicado por {b} é {a*b}")
+print(f"{a} dividido por b é {a/b}, mas com duas casas decímais é %.2f" %(a/b))
+print(f"{a} mais {b} é {a+b} e {a} menos {b} é {a-b}")
+print(f"{a} elevado a {b} {a**b} ")
---
+++ parapraticar2.py
@@ -0,0 +1,12 @@
+print("use números inteiros!")
+a = int(input("digite um número:"))
+b = int(input("digite um número:"))
+c = int(input("digite um número:"))
+
+delta = (b**2-(4*a*c))**(1/2)
+x1 = (-b+delta)/(2*a)
+x2 = (-b-delta)/(2*a)
+
+
+print(x1)
+print(x2)
